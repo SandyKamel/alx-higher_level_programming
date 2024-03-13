2173db800561b055d8e8831427302cbb8d565d7e
SandyKamel <SandyKamel@github.com>
2024-03-13 11:21:29 -0700

    commit 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = word [0:3]
 word_last_2 = word [-2]
-middle_word = word [1:3]
+middle_word = word [1:-1]
 print(f"first 3 letter :{word_first_3}")
 print(f"last 2 letter :{word_last_2}")
 print(f"middle word :{middle_word}")
