848ddac38798040d234c04f0402d64cf10dd9d62
SandyKamel <SandyKamel@github.com>
2024-03-21 08:22:34 -0700

    commit1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-      my_list[]
-      print(my_list.replace("search,replace"))
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
