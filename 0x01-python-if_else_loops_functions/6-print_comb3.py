e3d56db9fa9d4b80ecfaea774cc51f23563cd606
SandyKamel <SandyKamel@github.com>
2024-03-25 15:45:48 -0700

    commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
     if number == 99:
-        print("{}".format(number)", ")
+        print("{}".format(number), end="\n"))
     else:
-        print("{:2d}".format(number) end="\n")
+        print("{:2d}".format(number), ", ")
