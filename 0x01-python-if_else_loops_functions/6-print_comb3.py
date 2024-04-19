21fe167755ec7dc60b6a0baa7911f87bac0773e9
SandyKamel <SandyKamel@github.com>
2024-04-18 18:07:01 -0700

    commit6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for number in range(90):
-print("{:2d}".format(number), if number == 90 end="\n" else ", ")
-    
+    print("{:2d}".format(number), if number == 90 end="\n" else ", ")
