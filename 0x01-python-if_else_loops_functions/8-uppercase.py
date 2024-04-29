7d8e6baa4c52f1d1eac5c672062c97a198f41e9a
SandyKamel <SandyKamel@github.com>
2024-04-29 13:49:21 -0700

    commit 9

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for uppercase in str
-    upper_char = chr(ord(char - 32))
-    if ord(char) >= 97 and ord(char) <= 122:
-        print(upper_char)
+     for char in str:
+        upper_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
+        print(upper_char, end='')
+    print()
