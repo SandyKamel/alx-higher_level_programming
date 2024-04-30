8b4bad437299d079c4f2df89a35973231070c3a3
SandyKamel <SandyKamel@github.com>
2024-04-29 19:08:59 -0700

    commit10

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    lastdigit = abs(number) % 10
-print("{}".format(lastdigit), end="")
+    last = abs(number) % 10
+print("{}".format(last), end="")
